9dc011beabb6d3c4852a634a293d18270467a2fe
Sandesh Tiwari <sandeshtiwari@live.com>
2018-05-06 22:10:45 -0500

    feat: Make a dict to render the template

--- django_learning/demo_app/views.py
+++ django_learning/demo_app/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Hello World</h1>")
+def index(request):
+    my_dict = {'insert_here': 'This is the value to be inserted in from the template tag'}
+    return render(request, 'index.html', context=my_dict)
