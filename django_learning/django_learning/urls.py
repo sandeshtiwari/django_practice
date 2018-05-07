80ad6978a52b3e29d4aa88f329a9aaca474723b9
Sandesh Tiwari <sandeshtiwari@live.com>
2018-05-06 20:02:29 -0500

    feat: Add the view form the demo_app to the urlpatterns

--- django_learning/django_learning/urls.py
+++ django_learning/django_learning/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from demo_app import views
 
 urlpatterns = [
+    path('',views.index, name='demo'),
     path('admin/', admin.site.urls),
 ]
